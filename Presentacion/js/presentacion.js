d24ba47f477887d2f011bc382605f0d05b53cc21
DanielLopezZermeno <dani.lopez.zermeno@gmail.com>
2023-03-04 01:56:18 -0600

    canvas presentacion
    Agregue el fondo y el logo al canvas

--- Presentacion/js/presentacion.js
+++ Presentacion/js/presentacion.js
@@ -1,13 +1,27 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
 const musica = document.getElementById('musica');
 
-function iniciarJuego() {
-  // Ocultar la sección de inicio
-  document.querySelector('.inicio').style.display = 'none';
-  // Mostrar el canvas
-  canvas.style.display = 'block';
-  // Detener la música de la página de inicio
-  musica.pause();
+/* Crea una instancia de la imagen de fondo */
+let imgFondo = new Image();
+imgFondo.src = "img/fondo.jpg";
 
-}
+/* Espera a que la imagen cargue antes de usarla */
+imgFondo.onload = function() {
+   /* Dibuja la imagen en el canvas */
+   context.drawImage(imgFondo, 0, 0, canvas.width, canvas.height);
+};
+/* Crea una instancia de la imagen de logo */
+let imgLogo = new Image();
+imgLogo.src = "img/logo.png";
+
+/* Espera a que la imagen cargue antes de usarla */
+imgLogo.onload = function() {
+   /* Ajusta el tamaño de la imagen de acuerdo a tus necesidades */
+   let logoWidth = canvas.width * 0.2; // El ancho del logo será el 20% del ancho del canvas
+   let logoHeight = logoWidth * 0.5; // La altura del logo será la mitad de su ancho
+   
+   /* Dibuja el logo en la esquina inferior derecha del canvas */
+   context.drawImage(imgLogo, canvas.width - logoWidth - 650, canvas.height - logoHeight - 250, logoWidth, logoHeight);
+};
+
