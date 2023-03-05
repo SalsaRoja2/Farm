f6cc6534d3d62f61658f2281350be8936899baa5
DanielLopezZermeno <dani.lopez.zermeno@gmail.com>
2023-03-04 21:11:40 -0600

    Ediciones
    Edite la presentacion, el inicio

--- Presentacion/js/presentacion.js
+++ Presentacion/js/presentacion.js
@@ -18,10 +18,23 @@ imgLogo.src = "img/logo.png";
 /* Espera a que la imagen cargue antes de usarla */
 imgLogo.onload = function() {
    /* Ajusta el tamaño de la imagen de acuerdo a tus necesidades */
-   let logoWidth = canvas.width * 0.2; // El ancho del logo será el 20% del ancho del canvas
-   let logoHeight = logoWidth * 0.5; // La altura del logo será la mitad de su ancho
+   let logoWidth = canvas.width * .2; // El ancho del logo será el 20% del ancho del canvas
+   let logoHeight = logoWidth * 1; // La altura del logo será la mitad de su ancho
    
-   /* Dibuja el logo en la esquina inferior derecha del canvas */
-   context.drawImage(imgLogo, canvas.width - logoWidth - 650, canvas.height - logoHeight - 250, logoWidth, logoHeight);
+   context.drawImage(imgLogo, canvas.width - logoWidth - 630, canvas.height - logoHeight - 390, logoWidth, logoHeight);
+   context.font= "50px Cursive"
+   context.fillStyle= "black";
+   context.fillText("BIENVENIDO",610,500);
 };
+ 
+
+function empezar(){
+   let audio = new Audio('../sound/correcto.mp3');
+   // Reproducir el audio 
+   audio.play();
+   window.location.href="../Inicio/inicio.html"
+   // Crear un elemento audio en el documento HTML 
+   
+   
+}
 
